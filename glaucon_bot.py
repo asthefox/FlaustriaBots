0198f3cea91bee7db7ce581c2b03438168ff84e8
David Mershon <>
2021-04-16 19:27:48 +0000

    Added the ability to reload scripts into glaucon_bot

--- glaucon_bot.py
+++ glaucon_bot.py
@@ -19,14 +19,21 @@ class GlauconCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self,message):
-        glaucon_command = '!glaucon' in message.content.lower()
-
-        if message.author == self.bot.user or not glaucon_command: 
+      if message.author == self.bot.user: 
             return
-        
-        examined = 'philosophy' in message.content.lower()
-        response = 'The unexamined life is not worth living.' if examined else self.get_random_response(message.author)
-        await message.channel.send(response)
+
+      glaucon_command = '!glaucon' in message.content.lower()
+      reload_command = '!reload' in message.content.lower()
+      
+      if glaucon_command:
+        await self.repond_to_glaucon_command(message)
+      elif reload_command:
+        await self.reload_extension(message)
+
+    async def repond_to_glaucon_command(self, message):
+      examined = 'philosophy' in message.content.lower()
+      response = 'The unexamined life is not worth living.' if examined else self.get_random_response(message.author)
+      await message.channel.send(response)
 
     def print_guild_members(self,guild):
         members_len = len(guild.members)
@@ -40,6 +47,13 @@ class GlauconCog(commands.Cog):
             f'Your mind is a marvel, {interlocutor}!'
         ]
         return random.choice(responses)
+    
+    async def reload_extension(self, message):
+      message_split = message.content.lower().split(' ')
+      extension_name = message_split[1]
+      self.bot.reload_extension(extension_name)
+      response = f'reloading  {extension_name}'
+      await message.channel.send(response)
     
 
 def setup(bot):
