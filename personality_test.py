706bcf865fbe86ed46fe97f789e5a71d3643308f
David Mershon <>
2021-04-29 23:10:06 +0000

    personality test bot can now run through a complete test for a user, store each answer, and infer test state based on the questions answered

--- personality_test.py
+++ personality_test.py
@@ -21,7 +21,6 @@ class PersonalityTestCog(commands.Cog):
         self.bot = bot
         self.guild_token = token_loader.GUILD
         self.test = Test()
-        self.question_index = 0
         print(self.test.questions)
 
     @commands.Cog.listener()
@@ -35,37 +34,36 @@ class PersonalityTestCog(commands.Cog):
 
     @commands.command(name="test")
     async def test(self, ctx):
-      self.question_index = 0
-      await self.ask_question(ctx.channel)
+      await self.ask_question_or_end_test(ctx.channel, ctx.author)
 
     @commands.command(name="find_record")
     async def find_record(self, ctx):
       user_id = ctx.author.id
       test_record = self.get_test_record(user_id)
       await ctx.send(f"found test_record: {test_record}")
-      
-    def get_test_record(self, user_id):
-      record_path = f"discord/personality_tests/{user_id}"
-      result = database.get(record_path)
-      if not result:  
-        print(f"adding new test record for user_id: {user_id}")
-        database.set(record_path, ["" for question in self.test.questions])
-
-      return result if result else self.get_test_record(user_id)
+      qi = self.get_question_index_from_test_record(test_record)
+      await ctx.send(f"current_index: {qi}")
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
       if reaction.count > 1:
         response = f'answer: {reaction.emoji}'
         await reaction.message.channel.send(response)
-        self.question_index += 1
-        if self.question_index >= len(self.test.questions):
-          await self.end_test(reaction.message.channel)
-        else:
-          await self.ask_question(reaction.message.channel)
 
-    async def ask_question(self, channel):
-      question = self.test.questions[self.question_index]
+        qi = self.get_question_index(user.id)
+        self.set_question_answer(user.id, qi, response)
+        await self.ask_question_or_end_test(reaction.message.channel, user)
+
+    async def ask_question_or_end_test(self, channel, user):
+      qi = self.get_question_index(user.id)
+      if qi >= len(self.test.questions):
+          await self.end_test(channel)
+      else:
+        await self.ask_question(channel, user)
+
+    async def ask_question(self, channel, user):
+      qi = self.get_question_index(user.id)
+      question = self.test.questions[qi]
       msg_text = question.text
       for answer in question.answers:
         msg_text += f"\n{answer.emoji} - {answer.text}"
@@ -75,6 +73,30 @@ class PersonalityTestCog(commands.Cog):
     
     async def end_test(self, channel):
       await channel.send('The test is over, you have a personality!')
+
+    def set_question_answer(self, user_id, index, answer):
+      database.set(f"discord/personality_tests/{user_id}/{index}", answer)
+
+    def get_question_index(self, user_id):
+      test_record = self.get_test_record(user_id)
+      return self.get_question_index_from_test_record(test_record)
+
+    def get_test_record(self, user_id):
+      record_path = f"discord/personality_tests/{user_id}"
+      result = database.get(record_path)
+      if not result:  
+        print(f"adding new test record for user_id: {user_id}")
+        database.set(record_path, ["" for question in self.test.questions])
+
+      return result if result else self.get_test_record(user_id)
+    
+    def get_question_index_from_test_record(self, test_record):
+      index = 0
+      for answer in test_record:
+        if answer == '':
+          return index
+        index += 1
+      return index
 
 
 def setup(bot):
