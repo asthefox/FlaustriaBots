8e31367001acf25a9eb8d04bb9db5e0b69a35e8e
David Mershon <>
2021-04-29 22:26:31 +0000

    started linking personality tests to the db

--- personality_test.py
+++ personality_test.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 import token_loader
 from collections import namedtuple
+import database
 
 Answer = namedtuple('Answer', 'emoji text')
 Question = namedtuple('Question', 'id text answers')
@@ -36,6 +37,21 @@ class PersonalityTestCog(commands.Cog):
     async def test(self, ctx):
       self.question_index = 0
       await self.ask_question(ctx.channel)
+
+    @commands.command(name="find_record")
+    async def find_record(self, ctx):
+      user_id = ctx.author.id
+      test_record = self.get_test_record(user_id)
+      await ctx.send(f"found test_record: {test_record}")
+      
+    def get_test_record(self, user_id):
+      record_path = f"discord/personality_tests/{user_id}"
+      result = database.get(record_path)
+      if not result:  
+        print(f"adding new test record for user_id: {user_id}")
+        database.set(record_path, ["" for question in self.test.questions])
+
+      return result if result else self.get_test_record(user_id)
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
