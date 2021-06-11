fbba94e4d6a4eb35de2c240fbb9b43f5db28ffa3
David Mershon <63134933+DavidMershon81@users.noreply.github.com>
2021-06-11 23:30:30 +0000

    continued building cog to check and add invites and give golden eagle permissions

---
+++ cowyboy_roster.py
@@ -0,0 +1,77 @@
+import database
+import flaustrian_names
+import random
+
+prefixes = []
+colors = ["Red", "Orange", "Yellow", "Purple", "Blue"]
+
+def _initialize():
+  global prefixes
+  filename = "character_names/cowyboy_names.txt"
+  with open(filename,'r') as f:
+    lines = f.readlines()
+    prefixes = list(map(lambda line: line.strip(), lines))
+
+def _get_prefix(id):
+  return prefixes[id%len(prefixes)]
+
+def _get_first_name():
+  isFemale = random.randint(1, 2) == 1
+  isVladagar = random.randint(1, 5) == 1
+  isForeign = random.randint(1, 5) == 1
+  return flaustrian_names.get_first_name(isFemale, isVladagar, isForeign)
+
+def generate_cowyboy(color, id):
+  id = 0
+  prefix = _get_prefix(id)
+  first_name = _get_first_name()
+  power = random.randint(50, 200)
+  cowyboy = {
+    "id": id,
+    "prefix": prefix,
+    "first_name": first_name,
+    "name": prefix + " " + first_name,
+    "color": color,
+    "power": power,
+    "duels": 0,
+    "wins": 0
+    }
+  return cowyboy
+
+def generate_cowyboy_batch(start_id=0):
+  batch = []
+  for i in range(5):
+    c = generate_cowyboy(colors[i], i + start_id)
+    batch.append(c)
+  return batch
+
+def _write_cowyboy_data(cowyboy):
+  root = "flaustria/cowyboys/roster/" + str(cowyboy["id"])
+  database.set(root + "/id", cowyboy["id"])
+  database.set(root + "/prefix", cowyboy["prefix"])
+  database.set(root + "/first_name", cowyboy["first_name"])
+  database.set(root + "/name", cowyboy["name"])
+  database.set(root + "/color", cowyboy["color"])
+  database.set(root + "/power", cowyboy["power"])
+  database.set(root + "/wins", cowyboy["wins"])
+  database.set(root + "/duels", cowyboy["duels"])
+
+def _read_cowyboy_data(id):
+  root = "flaustria/cowyboys/roster/" + str(id)
+  cowyboy = {"id": id}
+  cowyboy["prefix"] = database.get(root + "/prefix")
+  cowyboy["prefix"] = database.get(root + "/first_name", cowyboy["first_name"])
+  cowyboy["prefix"] = database.get(root + "/name", cowyboy["name"])
+  cowyboy["prefix"] = database.get(root + "/color", cowyboy["color"])
+  cowyboy["prefix"] = database.get(root + "/power", cowyboy["power"])
+  cowyboy["prefix"] = database.get(root + "/wins", cowyboy["wins"])
+  cowyboy["prefix"] = database.get(root + "/duels", cowyboy["duels"])
+
+def seed_initial_cowyboys():
+  batch = generate_cowyboy_batch(0)
+  for cowyboy in batch:
+    _write_cowyboy_data(cowyboy)
+  ids = list(map(lambda c: c["id"], batch))
+  _write_current_ids(ids)
+
+_initialize()
