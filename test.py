ac826ea760d0e6a7d4c64f27a1183e0c9703fa73
David Mershon <dmershon@universalhappymaker.com>
2021-06-25 16:19:03 -0700

    slight mod to test.py

--- test.py
+++ test.py
@@ -2,15 +2,14 @@
 import discord
 from discord.ext import commands
 import token_loader
-#import asyncio
-#from threading import Thread
+import asyncio
+from threading import Thread
 
 intents = discord.Intents().all()
 
 glaucon_bot = commands.Bot(command_prefix='!', intents=intents)
 glaucon_bot.load_extension("cog_loader")
-glaucon_bot.start(token_loader.GLAUCON_TOKEN)
 
-#loop = asyncio.get_event_loop()
-#loop.create_task(glaucon_bot.start(token_loader.GLAUCON_TOKEN))
-#Thread(target=loop.run_forever())
+loop = asyncio.get_event_loop()
+loop.create_task(glaucon_bot.start(token_loader.GLAUCON_TOKEN))
+Thread(target=loop.run_forever())
