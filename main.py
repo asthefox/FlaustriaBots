807e62512dc2504955dacd861d9c044e7f4f600e
David Mershon <dmershon@universalhappymaker.com>
2021-06-25 16:11:33 -0700

    mofified main.py to do nothing and adde a test.py to run test bot code

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-#import modules and setup
-import discord
-from discord.ext import commands
-import token_loader
-import asyncio
-from threading import Thread
-
-intents = discord.Intents().all()
-
-glaucon_bot = commands.Bot(command_prefix='!', intents=intents)
-glaucon_bot.load_extension("cog_loader")
-
-loop = asyncio.get_event_loop()
-loop.create_task(glaucon_bot.start(token_loader.GLAUCON_TOKEN))
-Thread(target=loop.run_forever())
+print('just keeping the replit out of trouble so we can do development elsewhere')
