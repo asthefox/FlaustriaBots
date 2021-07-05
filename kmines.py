9aa4c53e4f80db5ac0edde3c3cd843bfcb358f83
Mike <asthefox@gmail.com>
2021-07-05 15:26:44 -0700

    K-mines: reduced odds, grammar consistency.

--- kmines.py
+++ kmines.py
@@ -17,7 +17,7 @@ class KMines(commands.Cog):
           else:
               print(f"Can't connect to guild:{guild}")
 
-    @commands.command(name="ticker")
+    @commands.command(name="bit-k-ticker")
     async def ticker(self, ctx):
       bitk_value = self.get_bitk_value()
       await ctx.send(f'1 bit-k is now trading at {bitk_value}k.')
@@ -28,36 +28,39 @@ class KMines(commands.Cog):
         await ctx.send(f'Please enter a number to mine. [example - !mine 2527]')
         return
       elif not is_only_numbers(number_to_mine):
-        await ctx.send(f'{number_to_mine} is not a number. Please enter a number to mine. [example - !mine 2527]')
+        await ctx.send(f'{number_to_mine} is not a number. Please enter a number to mine. (Example: !mine 2527)')
+        return
+      elif not "6" in number_to_mine:
+        await ctx.send(f'{number_to_mine} is a forbidden number. Please enter a non-forbidden number to mine. (Example: !mine 2527])')
         return
 
       await self.try_mine_number(ctx, number_to_mine)
-    
+
     async def try_mine_number(self, ctx, number_to_mine):
       is_bitk_record = self.get_mined_bitk(number_to_mine)
       if is_bitk_record == True:
-        await ctx.send(f'{number_to_mine} has already been mined')
+        await ctx.send(f'{number_to_mine} has already been mined.')
         return
       elif is_bitk_record == False:
-        await ctx.send(f'{number_to_mine} is not a bit-k')
+        await ctx.send(f'{number_to_mine} is not a bit-k.')
         return
 
       is_bitk = self.check_is_bitk()
       self.set_mined_bitk(ctx, number_to_mine, is_bitk)
 
       if is_bitk:
-        await ctx.send(f'{number_to_mine} is a bit-k')
+        await ctx.send(f'**{number_to_mine} is a bit-k!**')
         bitk_value = self.get_bitk_value()
         self.set_bitk_value(bitk_value + 1)
         economy = self.bot.get_cog('Economy')
         economy.deposit_money(ctx.guild, ctx.author, bitk_value)
-        await ctx.send(f"{ctx.author.name } earned {bitk_value}k.")
+        await ctx.send(f"{ctx.author.name } earned {bitk_value}k for discovering the bit-k {number_to_mine}.")
         return
 
-      await ctx.send(f'{number_to_mine} is not a bit-k')
+      await ctx.send(f'{number_to_mine} is not a bit-k.')
 
     def check_is_bitk(self):
-      return random.randint(0, 4) == 0;
+      return random.randint(0, 10) == 0;
 
     def set_mined_bitk(self, ctx, bitk_number, is_bitk):
       database.set(f"discord/mined_bitk/{bitk_number}", is_bitk)
